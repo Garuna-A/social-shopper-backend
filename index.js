e4ac7c34a9e2f6a378634e31c8899cf1657efb96
Garuna-A <mishra.anuragdli@gmail.com>
2026-06-22 13:33:40 +0530

    Add packages for AI search

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const roomRoutes = require('./routes/room');
 const itemRoutes = require('./routes/items');
 const ebayRoutes = require('./routes/ebay');
-
+const aiRoutes = require('./routes/aiRoutes');
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +14,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/rooms', roomRoutes);
 app.use('/api/ebay',ebayRoutes);
 app.use('/api/items', itemRoutes);
+app.use('/api/ai',aiRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
